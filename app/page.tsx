5541f39be53b998ccb5152655c87705cb5f571c8
jsays <jsays.citadel@gmail.com>
2024-04-26 00:04:43 -0500

    placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Page() {
     return (
-      <div>
-        <div id="header">
-          <div id="logo">
-            <img id="meh_logo" src="/images/meh.png" alt="Meh Logo" />
-          </div>
-          <div id="menu"><FontAwesomeIcon icon={['fas', 'bars']} size="2xl" /></div>
+        <div>
+            <div id="header">
+                <div id="logo">
+                    <img id="meh_logo" src="/images/meh.png" alt="Meh Logo" />
+                </div>
+                <div id="menu"><FontAwesomeIcon icon={['fas', 'bars']} size="2xl" /></div>
+            </div>
+            <Products />
+            <div id="footer"></div>
         </div>
-        <h1>Meh voting</h1>
-        <h1 className="text-3xl font-bold underline">TailwindCSS test</h1>
-      </div>
     );
-  }
+}
+
+// Connect Wallet
+// Set Chain to Base
+// Add Token to Wallet
+// Indicate connection status
+
+function Product() {
+    return (
+        <div className="basis-1/2">
+            <div className="basis-1/2 border-solid border border-gray-400 m-8 p-8">
+                Product
+            </div>
+        </div>
+    )
+};
+
+function Products() {
+    return (
+        <div className='flex flex-wrap w-screen'>
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+        </div>
+    )
+};
