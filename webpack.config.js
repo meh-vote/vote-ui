890762db92d5ef5ab1caffba910bd0c64c2ed04c
jsays <jsays.citadel@gmail.com>
2024-05-10 02:32:11 -0500

    Output of this build now matches current state of meh.vote site

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/vote.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-  ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  mode: 'none',
+    mode: 'none',
+    entry: './src/vote.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/css', to: 'css' },
+                { from: 'src/images', to: 'images' },
+                { from: 'src/favicon.ico', to: 'favicon.ico' },
+            ]
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
 };
